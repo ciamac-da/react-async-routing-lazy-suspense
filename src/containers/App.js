4b2ec4cb0418ec9882fc71bdf2a9e31e3fd9692a
ciamac-da <ciamac.davoudi@gmail.com>
2021-04-11 09:59:21 +0200

    Verified by Marti4n

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,14 +4,29 @@ import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 //import User from '../components/User';
 //import Welcome from '../components/Welcome';
 const Welcome = React.lazy(()=>import("../components/Welcome.js"))
-const Posts = React.lazy(()=>import("../components/Posts.js"))
-const User = React.lazy(()=>import("../components/User.js"))
+const Posts   = React.lazy(()=>import("../components/Posts.js"))
+const User    = React.lazy(()=>import("../components/User.js"))
 
 
 class App extends Component {
+ /*  state={
+    showPosts:false
+  }
+ */
+  /* modeHandler=()=>{
+    this.setState(prevState=>{
+      return {showPosts: !prevState.showPosts}
+    })
+  } */
   render() {
     return (
-      <BrowserRouter>
+   /*    <React.Fragment>
+      <button onClick={this.modeHandler}>Toggle Mode</button>
+      {this.state.showPostsow ? ( <Suspense fallback={<div>Loading Posts...</div>}>
+      <Posts />
+    </Suspense>): <User />}
+      </React.Fragment> */
+       <BrowserRouter>
         <React.Fragment>
           <nav>
             <NavLink to="/user">User Page</NavLink> &nbsp;
@@ -46,7 +61,7 @@ class App extends Component {
           )}
           />
         </React.Fragment>
-      </BrowserRouter>
+      </BrowserRouter>  
     );
   }
 }
